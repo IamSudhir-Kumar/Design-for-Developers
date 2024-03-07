137c98af1bce28320d5429ce7c4cc5a8f8c2fe6e
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-03-07 20:20:32 +0530

    Delete unnecessary files and configurations

--- src/appwrite/authentication.js
+++
@@ -1,7 +0,0 @@
-import conf from "../config/config.js";
-import { Client,Account, ID } from "appwrite";
-
-export class Authentication {}
-const authentication = new Authentication();
-
-export default Authentication;
