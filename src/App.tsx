f4c3ea07019aab7f19991a5f367ed40aeb8ce877
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-03-10 13:54:52 +0530

    Refactor App.tsx and Navbar.tsx components to add routing and update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import Navbar from "./components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Logs from "./components/Logs/Logs";
+import Metrics from "./components/Metrics/Metrics";
 
 function App() {
   return (
-   <Navbar />
-  )
+    <>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/logs" element={<Logs />} />
+          <Route path="/metrics" element={<Metrics />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Metrics/Metrics.tsx
+++ src/components/Metrics/Metrics.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Metrics: React.FC = () => {
   return (
-    <div className="container mx-auto">
-      {/* Add components for charts and time range based filtering */}
+    <div className="grid grid-cols-5 grid-rows-5 gap-4 m-2">
+      <div className="col-span-5 justify-center p-2 text-xl font-bold border-solid border-2 rounded-sm">Metrics</div>
+      <div className="col-span-2 row-span-2 row-start-2">2</div>
+      <div className="col-span-2 row-span-2 col-start-4 row-start-2">3</div>
+      <div className="col-span-2 row-span-2 row-start-4">4</div>
+      <div className="col-span-2 row-span-2 col-start-4 row-start-4">5</div>
     </div>
   );
 };
